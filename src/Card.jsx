c98e72af123a0141955a39da056db2780cbaf0ba
Bowen Suico <bowenhsuico@gmail.com>
2024-03-15 00:38:10 +0800

    Final

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,6 @@
 import Pic from './assets/pic.png';
-//import PropTypes from 'prop-types';
 function Card(props){
 
-//    const studentList = props.lists;
-
-//    const listStudents = studentList.map(student => <li key={student.id} > {student.name} {student.email}</li>);
-
-//   const students = [   {id: 1, name: "Bowen Suico", email: "bowenhsuico@gmail.com"},
-  //                      {id: 2, name: "Krystal June Codeniera", email: "krystalization@gmail.com"},
-    //                    {id: 3, name: "Jude Michael Dagatan", email: "bowenhsuico@gmail.com"},
-      //                  {id: 4, name: "Bowssing", email: "bowenhsuico@gmail.com"},
-        //               {id: 5, name: "Bowssing", email: "bowenhsuico@gmail.com"}];
 
     return(
         <div className="card">
